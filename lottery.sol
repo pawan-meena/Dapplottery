a03f68d099bca3db0a53429e18e9108feb103e1b
pawan <pawanmeenazee@gmail.com>
2022-03-10 21:08:59 +0530

    check

--- lottery.sol
+++ lottery.sol
@@ -1,34 +1,50 @@
-// SPDX-License-Identifier: GPL--3.0
-pragma solidity >= 0.8.0;
-contract lottery
-{
-  address public manager;
-  address payable[] public participants;
-  constructor()
-   {
-     manager=msg.sender;//global variable
+//SPDX-License-Identifier: GPL-3.0
 
-   }
-  receive() external payable{
-    require(msg.value==1 ether);
-    participants.push(payable(msg.sender));
-  
-  }
-function getB() public view returns(uint){
-  require(msg.sender== manager);
-  return address(this).balance;
-}
+pragma solidity >=0.5.0 <0.9.0;
 
-function rend() public view returns(uint){
-return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, participants.length)));
-}
-function selectwin() public{
-  require(msg.sender==manager);
-  require(participants.length>=3);
-  uint r=rend();
-  address payable winer;
-  uint index = r % participants.length;
- winer=participants[index];
- winer.transfer(getBalance());
-}
+contract Lottery{
+
+    address payable[] public players;
+    address public manager;
+
+
+
+    constructor(){
+        manager = msg.sender;
+    }
+
+    receive () payable external{
+        require(msg.value == 0.1 ether);
+        players.push(payable(msg.sender));
+    }
+
+    function getBalance() public view returns(uint){
+        require(msg.sender == manager);
+        return address(this).balance;
+    }
+
+    function random() internal view returns(uint){
+       return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players.length)));
+    }
+
+
+    function pickWinner() public{
+
+        require(msg.sender == manager);
+        require (players.length >= 3);
+
+        uint r = random();
+        address payable winner;
+
+
+        uint index = r % players.length;
+
+        winner = players[index];
+
+        winner.transfer(getBalance());
+
+
+        players = new address payable[](0);
+    }
+
 }
